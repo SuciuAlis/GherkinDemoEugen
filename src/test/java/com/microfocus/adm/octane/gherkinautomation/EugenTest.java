a7a23bddd5d3c9d4fe098a2f456eba84a1e303f5
EFlocea <eugen.flocea@microfocus.com>
2023-02-22 14:40:13 +0200

    Try smt on EugenTest

--- src/test/java/com/microfocus/adm/octane/gherkinautomation/EugenTest.java
+++ src/test/java/com/microfocus/adm/octane/gherkinautomation/EugenTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/Eugen_OctaneGherkinResults.xml"},
-        features="src/test/resources/eugen")
+@CucumberOptions(plugin={"com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/Elisheva_OctaneGherkinResults.xml"},
+        features="src/test/resources/elisheva")
 public class EugenTest {
 }
